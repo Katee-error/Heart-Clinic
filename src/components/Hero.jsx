276421c3149ea737c8aa0fd50ee26e7577e15d2b
Ekaterina <kate8190758@gmail.com>
2024-08-14 14:51:12 +0300

    /

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Container } from '@chakra-ui/react'
 
 
 const Hero = () => {
   return (
     <Box>
-
+        <Container maxW="container.xl">
+            <Box bg={'#F2F2F7'} w={'100%'}  h={'500px'} borderRadius={'20px'}>
+                
+            </Box>
+        </Container>
     </Box>
   )
 }
