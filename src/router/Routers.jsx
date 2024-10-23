b234eecf5bad6108519ac96ab9364d5fec2db789
Ekaterina <kate8190758@gmail.com>
2024-10-23 23:38:03 +0400

    fix

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,26 +1,27 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "../pages/Home";
-import ServicesCostPage from "../pages/ServicesCostPage";
-import AboutUsPage from "../pages/AboutUsPage";
-import OmsPage from "../pages/OmsPage";
-import ContactPage from "../pages/ContactPage";
-import DoctorsPages from "../pages/DoctorsPages";
-import Promotion from "../pages/Promotion";
-import PromotionDetail from "../pages/PromotionDetail";
+import { lazy } from "react";
+import Home from "./../pages/Home";
+const ServicesCostPage = lazy(() => import("../pages/ServicesCostPage"));
+const AboutUsPage = lazy(() => import("../pages/AboutUsPage"));
+const OmsPage = lazy(() => import("../pages/OmsPage"));
+const ContactPage = lazy(() => import("../pages/ContactPage"));
+const DoctorsPages = lazy(() => import("../pages/DoctorsPages"));
+const Promotion = lazy(() => import("../pages/Promotion"));
+const PromotionDetail = lazy(() => import("../pages/PromotionDetail"));
 
 const Routers = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/servicesCost" element={<ServicesCostPage />} />
-        <Route path="/aboutUs" element={<AboutUsPage />} />
-        <Route path="/promotion" element={<Promotion />} />
-        <Route path="/promotion/:id" element={<PromotionDetail />} />
-        <Route path="/oms" element={<OmsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/doctors" element={<DoctorsPages />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/servicesCost" element={<ServicesCostPage />} />
+      <Route path="/aboutUs" element={<AboutUsPage />} />
+      <Route path="/promotion" element={<Promotion />} />
+      <Route path="/promotion/:id" element={<PromotionDetail />} />
+      <Route path="/oms" element={<OmsPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/doctors" element={<DoctorsPages />} />
+    </Routes>
   );
 };
 
