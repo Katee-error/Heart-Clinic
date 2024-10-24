f8562f47d25cf81ff10f7a58a561435b90ae392d
Ekaterina <kate8190758@gmail.com>
2024-10-24 19:56:42 +0400

    resize slide

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -14,9 +14,20 @@ import slide02 from "./../assets/slider/slide-4.webp";
 import slide04 from "./../assets/slider/slide-5.webp";
 import slide05 from "./../assets/slider/slide-6.webp";
 
+import slideSmall01 from "./../assets/slider/slideSmall-1.webp";
+import slideSmall03 from "./../assets/slider/slideSmall-2.webp";
+import slideSmall02 from "./../assets/slider/slideSmall-4.webp";
+import slideSmall04 from "./../assets/slider/slideSmall-5.webp";
+import slideSmall05 from "./../assets/slider/slideSmall-6.webp";
+
 const HeroSlider = () => {
-  
+
   const [isMobile] = useMediaQuery("(max-width: 768px)");
+
+  
+  const desktopImages = [slide01, slide02, slide03, slide04, slide05];
+  const mobileImages = [slideSmall01, slideSmall03, slideSmall02, slideSmall04,slideSmall05]; 
+const images = isMobile ? mobileImages : desktopImages;
   const settings = {
     dots: false,
     infinite: true,
@@ -29,25 +40,21 @@ const HeroSlider = () => {
     cssEase: "ease",
     pauseOnHover: false,
     lazyLoad: "ondemand",
-    beforeChange: (current, next) => handleLazyLoad(next), // Обработчик смены слайдов
+    beforeChange: (current, next) => handleLazyLoad(next),
   };
 
   const sliderRef = useRef(null);
-  const images = [slide01, slide02, slide03, slide04, slide05];
   const MotionButton = motion(Button);
-  const [isModalOpen, setIsModalOpen] = useState(false); // Модальное окно
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [imagesLoaded, setImagesLoaded] = useState(false);
-  const [loadedImages, setLoadedImages] = useState([0, 1]); // Индексы загруженных изображений
+  const [loadedImages, setLoadedImages] = useState([0, 1, 2]); // Load first three images initially
 
   // Функция для ленивой загрузки изображений
   const handleLazyLoad = (nextIndex) => {
-    // Если индекс следующего слайда больше текущего количества загруженных изображений,
-    // добавляем его в список для загрузки
     if (!loadedImages.includes(nextIndex)) {
       setLoadedImages((prev) => [...prev, nextIndex]);
     }
 
-    // Если мы близки к концу слайдов, подгружаем последние слайды
     if (nextIndex >= images.length - 3) {
       setLoadedImages((prev) =>
         [
@@ -63,18 +70,29 @@ const HeroSlider = () => {
   useEffect(() => {
     const loadInitialImages = () => {
       let loaded = 0;
-      const firstTwoImages = images.slice(0, 1); // Загрузить только первые 2 изображения
+      const initialImages = images.slice(0, 3); // Load the first 3 images
 
-      firstTwoImages.forEach((image) => {
+      initialImages.forEach((image) => {
         const img = new window.Image();
         img.src = image;
         img.onload = () => {
           loaded++;
-          if (loaded === firstTwoImages.length) {
+          if (loaded === initialImages.length) {
             setImagesLoaded(true);
           }
         };
       });
+
+      // Load remaining images with a delay
+      for (let i = 3; i < images.length; i++) {
+        setTimeout(() => {
+          const img = new window.Image();
+          img.src = images[i];
+          img.onload = () => {
+            setLoadedImages((prev) => [...prev, i]); // Add to loaded images
+          };
+        }, 500 * (i - 3)); // 500ms delay for each image after the first three
+      }
     };
 
     loadInitialImages();
@@ -94,7 +112,6 @@ const HeroSlider = () => {
       <Slider {...settings} ref={sliderRef}>
         {images.map((image, index) => (
           <Box key={index} position="relative">
-            {/* Отображаем изображение только если оно загружено */}
             {loadedImages.includes(index) && (
               <Image
                 src={image}
@@ -102,7 +119,7 @@ const HeroSlider = () => {
                 width="100%"
                 height="100%"
                 borderRadius={"20px"}
-                loading="lazy" // Атрибут для ленивой загрузки
+                loading="lazy"
               />
             )}
             <Box
