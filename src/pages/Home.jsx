742599ff15f476e54ddb8ff1c56e87693b223c4b
Ekaterina <kate8190758@gmail.com>
2024-10-24 17:37:31 +0400

    chamge yandex metrica

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { Box } from "@chakra-ui/react";
 import HeroSlider from "../components/HeroSlider";
-import AboutUs from "../components/AboutUs";
+// import AboutUs from "../components/AboutUs";
 import OurDoctors from "../components/DoctorsList";
 import ContactForm from "../components/FormTest";
 import YandexMap from "../components/YandexMap";
@@ -8,19 +9,20 @@ import Benefits from "../components/Benefits";
 import Test from "../components/Test";
 import TestimonialsMarquee from "./../components/Reviews";
 import PromotionList from "../components/PromotionList";
+const AboutUs = React.lazy(() => import("../components/AboutUs"));
 
 const Home = () => {
   return (
     <Box>
-    <HeroSlider />
-    <AboutUs />
-    <Benefits />
-    <Test />
-    <OurDoctors />
-    <PromotionList />
-    <TestimonialsMarquee />
-    <ContactForm />
-    <YandexMap />
+      <HeroSlider />
+      <AboutUs />
+      <Benefits />
+      <Test />
+      <OurDoctors />
+      <PromotionList />
+      <TestimonialsMarquee />
+      <ContactForm />
+      <YandexMap />
     </Box>
   );
 };
